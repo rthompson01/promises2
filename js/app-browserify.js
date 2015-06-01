08735acf91682872442d475cffc405dcd6a502d6
Rey Thompson <rthompson1@gmail.com>
2015-06-01 13:59:54 -0500

    initial commit

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -1,26 +1,29 @@
 "use strict";
 
-// es5 polyfills, powered by es5-shim
-require("es5-shim")
-// es6 polyfills, powered by babel
-require("babel/register")
-
 var Promise = require('es6-promise').Promise
 // just Node?
 // var fetch = require('node-fetch')
 // Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
+require('whatwg-fetch') //--> not a typo, don't store as a var
 
-// other stuff that we don't really use in our own code
-// var Pace = require("../bower_components/pace/pace.js")
+// es6 polyfills, powered by babel
+require("babel/register")
 
-// require your own libraries, too!
-// var Router = require('./app.js')
+var urls = [ 'https://api.github.com/users/rthompson01','https://api.github.com/users/rthompson01/repos' ]
 
-// window.addEventListener('load', app)
+var promises = urls.map((url) => fetch(url).then((r) => r.json()))
 
-// function app() {
-    // start app
-    // new Router()
-// }
+Promise.all(requests).then((data) => {
+    var profile = data[0],
+    	repos = data [1]
 
+
+    var profile_string = ['name', 'login', 'blog', 'location', 'email',
+    	'html_url'].map((key) =>  `<li>${key}: ${profile[key]}</li>).join('')
+    var repo_string = repos.map((repo) => `<li><a href='${repo.name}</a></li>').join('')
+
+    qs('.profile img').src = profile.avatar_url
+    qs('.profile ul').innerHTML = profile_string
+    qs('.repos ul').innerHTML = repo_string
+
+})
